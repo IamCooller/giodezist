882a81995d4f3fc318811a120c8e67eaee25d5b7
IamCooller <cooller2000@inbox.ru>
2022-06-06 17:34:15 +0300

    Update sleeping_owl.php

--- config/sleeping_owl.php
+++ config/sleeping_owl.php
@@ -156,7 +156,7 @@ return [
     |--------------------------------------------------------------------------
     */
 
-    'imagesUploadDirectory' => 'images/uploads',
+    'imagesUploadDirectory' => 'public/images/uploads',
 
     /*
     |--------------------------------------------------------------------------
@@ -201,7 +201,7 @@ return [
     |--------------------------------------------------------------------------
     */
 
-    'filesUploadDirectory' => 'files/uploads',
+    'filesUploadDirectory' => 'public/files/uploads',
 
     /*
     |--------------------------------------------------------------------------
@@ -271,6 +271,8 @@ return [
             'height'          => 200,
             'allowedContent'  => true,
             'extraPlugins'    => 'uploadimage,image2,justify,youtube,uploadfile',
+            
+            'imagesUploadDirectory' => 'public/images/uploads',
             /*
              * WARNING!!!! CKEDITOR on D & D and UploadImageDialog
              * BY DEFAULT IMAGES WILL STORE TO imagesUploadDirectory = /images/uploads
